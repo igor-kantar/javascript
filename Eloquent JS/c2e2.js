01d166798a6fc633f54573f7ed10125194fa3b10
Igor Kantar <igor_kan_bl@hotmail.com>
2022-06-17 00:09:19 +0000

    "Debloated"

--- Eloquent JS/c2e2.js
+++ Eloquent JS/c2e2.js
@@ -1,20 +1,12 @@
-const fb = "FizzBuzz";
-const f  = "Fizz";
-const b  = "Buzz";
+const fb = "FizzBuzz"
+const f  = "Fizz"
+const b  = "Buzz"
 
 for (let i=1; i<101; i++){
     if (i % 5 == 0){
-        if (i % 3 == 0){
-            console.log(fb)
-        }
-        else{
-        console.log(b)
-        }
+        if (i % 3 == 0){ console.log(fb)}
+        else{ console.log(b) }
     }
-    else if (i % 3 == 0){
-        console.log(f)
-    }
-    else{
-        console.log(i)
-    }
+    else if (i % 3 == 0){ console.log(f) }
+    else{ console.log(i) }
 }
