70044593fbfcb444aa5273a8b6b825a4f8d07ed0
Igor Kantar <igor_kan_bl@hotmail.com>
2022-06-19 21:41:46 +0000

    Looping a triangle

--- Eloquent JS/c2e1.js
+++ Eloquent JS/c2e1.js
@@ -1,3 +1,13 @@
+// Write a loop that makes seven calls to console.log to output the following
+// triangle:
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
 for(let s="#", i=0; i<7; i++, s+="#"){
     console.log(s);
 }
