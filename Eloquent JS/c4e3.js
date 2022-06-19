fad276f2189604d21252f80fb966d97de16a5781
Igor Kantar <igor_kan_bl@hotmail.com>
2022-06-19 22:33:10 +0000

    A list

--- Eloquent JS/c4e3.js
+++ Eloquent JS/c4e3.js
@@ -18,46 +18,46 @@
 //  If you haven’t already, also write a recursive version of nth.
 
 function arrayToList(arr){
-    let list = null;
+    let list = null
     for (let i = arr.length - 1; i >= 0; i--)
-        list = {value: arr[i], rest: list};
-  return list;
+        list = {value: arr[i], rest: list}
+  return list
 }
 
 function listToArray(list){
-    let arr = [];
+    let arr = []
     do{
-        arr.push(list.value);
-        list = list.rest;
+        arr.push(list.value)
+        list = list.rest
     }while(list.rest != null)
     arr.push(list.value)
 
-    return arr;
+    return arr
 }
 
 const prepend = (elem, list) => Object({value:elem, rest:list})
 
 function nth(list, number){
-    arr = listToArray(list);
-    return arr[number];
+    arr = listToArray(list)
+    return arr[number]
 }
 
 function recursiveNth(list, number){
     if (number == 0){
-        return list.value;
+        return list.value
     }else
         return recursiveNth(list.rest, --number);
 }
 
-list = arrayToList([1,2,3]);
+list = arrayToList([1,2,3])
 
 console.log("ArrayToList: "+ `{value: ${list.value}, rest: { value: ${list.rest.value}, rest: { value: ${list.rest.rest.value}, rest: ${list.rest.rest.rest}}}}`)
 // -> ArrayToList: {value: 1, rest: { value: 2, rest: { value: 3, rest: null}}}
-console.log("ListToArray: " + listToArray(list));
+console.log("ListToArray: " + listToArray(list))
 // -> ListToArray: 1,2,3
-console.log("Prepend Zero: "+ listToArray(prepend(0,list)));
+console.log("Prepend Zero: "+ listToArray(prepend(0,list)))
 // -> Prepend Zero: 0,1,2,3
-console.log("Nth: " + nth(list, 2));
+console.log("Nth: " + nth(list, 2))
 // -> Nth: 3
-console.log("RecursiveNth: " + recursiveNth(list, 2));
+console.log("RecursiveNth: " + recursiveNth(list, 2))
 // -> RecursiveNth: 3
