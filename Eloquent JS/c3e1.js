346b99d868bb3d546459bee10990586b2f5e2e5c
Igor Kantar <igor_kan_bl@hotmail.com>
2022-06-19 21:49:51 +0000

    Minimum

--- Eloquent JS/c3e1.js
+++ Eloquent JS/c3e1.js
@@ -1,5 +1,10 @@
-function min(a, b){
-    return a < b ? a : b;
-}
+// The previous chapter introduced the standard function Math.min that returns
+// its smallest argument. We can build something like that now. Write a function
+// min that takes two arguments and returns their minimum.
 
+const min = (a, b) =>  a < b ? a : b
+
+console.log(min(12,24))
+// -> 12
 console.log(min(12,6))
+// -> 6
