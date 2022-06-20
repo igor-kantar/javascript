7dbb7aafe9a9c07a96a7f4ef0d3d2ce9f788f612
Igor Kantar <igor_kan_bl@hotmail.com>
2022-06-20 20:52:23 +0000

    Everything

--- Eloquent JS/c5e3.js
+++ Eloquent JS/c5e3.js
@@ -1,3 +1,11 @@
+// Analogous to the some method, arrays also have an every method. This one
+// returns true when the given function returns true for every element in the array.
+// In a way, some is a version of the || operator that acts on arrays, and every is
+// like the && operator.
+//  Implement every as a function that takes an array and a predicate function
+// as parameters. Write two versions, one using a loop and one using the some
+// method.
+
 function every(arr, testf) {
     for (const elem of arr) {
         if (!testf(elem))
@@ -6,22 +14,16 @@ function every(arr, testf) {
     return true
 }
 
-function every2(arr, testf) {
-    // de morgans theorem
-    return !arr.some(elem => !testf(elem))
-}
+// de morgans theorem
+const everySome = (arr, testf) => !arr.some(elem => !testf(elem))
 
-function testf(num) {
-    if(num % 2 == 0)
-        return true
-    return false
-}
+const testf = (num) => num % 2 == 0
 
 console.log(every([2,4,6,8],testf))
 // -> true
 console.log(every([2,4,6,7],testf))
 // -> false
-console.log(every2([2,4,6,8],testf))
+console.log(everySome([2,4,6,8],testf))
 // -> true
-console.log(every2([2,4,6,7],testf))
+console.log(everySome([2,4,6,7],testf))
 // -> false
