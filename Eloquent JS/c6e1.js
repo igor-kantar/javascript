556b5122cf920a9822aaf05d32ac85bd4c4dfaf3
Igor Kantar <igor_kan_bl@hotmail.com>
2022-06-20 20:59:34 +0000

    A vector type

--- Eloquent JS/c6e1.js
+++ Eloquent JS/c6e1.js
@@ -1,3 +1,12 @@
+// Write a class Vec that represents a vector in two-dimensional space. It takes
+// x and y parameters (numbers), which it should save to properties of the same
+// name.
+//  Give the Vec prototype two methods, plus and minus, that take another
+// vector as a parameter and return a new vector that has the sum or difference
+// of the two vectors’ (this and the parameter) x and y values.
+//  Add a getter property length to the prototype that computes the length of
+// the vector—that is, the distance of the point (x, y) from the origin (0, 0).
+
 class Vec{
     constructor(x, y){
         this.x = x
@@ -9,18 +18,18 @@ class Vec{
     minus(vec){
         return new Vec(this.x-vec.x,this.y-vec.y)
     }
-    getLength(){
+    length(){
         return Math.sqrt(this.x*this.x + this.y*this.y)
     }
 }
 
-v1 = new Vec(1,1)
-v2 = new Vec(2,3)
+const v1 = new Vec(1,1)
+const v2 = new Vec(2,3)
 
-sum = v1.plus(v2)
-dif = v1.minus(v2)
+let sum = v1.plus(v2)
+let dif = v1.minus(v2)
 
-console.log(`Len: ${v1.getLength()}`)
+console.log(`Len: ${v1.length()}`)
 // -> 1.4142135623730951
 console.log(`Sum: ${sum.x},${sum.y}`);
 // -> Sum: 3,4
