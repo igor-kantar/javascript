d03b93cf9455bc9e3c144ebd3a1b843d95c6b277
Igor Kantar <igor_kan_bl@hotmail.com>
2022-06-20 20:45:56 +0000

    Flattening

--- Eloquent JS/c5e1.js
+++ Eloquent JS/c5e1.js
@@ -1,9 +1,8 @@
+// Use the reduce method in combination with the concat method to “flatten”
+// an array of arrays into a single array that has all the elements of the original
+// arrays.
+
 let arr = [[0,1,2],[3,4,5],[6,7,8]]
 
-function flatten(arr1, arr2){
-    let flatArr = arr1.concat(arr2)
-    return flatArr
-
-}
-console.log(arr.reduce(flatten))
+console.log(arr.reduce(((arr1, arr2) => arr1.concat(arr2))))
 // -> [0,1,2,3,4,5,6,7,8]
